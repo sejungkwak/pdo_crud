3ea60d79e0cd23febf9dc20fafa1e773ab8a54a0
sejungkwak <kwak.sejung@gmail.com>
2025-10-19 18:05:48 +0100

    Create edit buttons

--- public/update.php
+++ public/update.php
@@ -30,6 +30,7 @@ try {
             <th>Age</th>
             <th>Location</th>
             <th>Date</th>
+            <th>Edit</th>
         </tr>
     </thead>
     <tbody>
@@ -42,6 +43,7 @@ try {
             <td><?php echo escape($row["age"]); ?></td>
             <td><?php echo escape($row["location"]); ?></td>
             <td><?php echo escape($row["date"]); ?></td>
+            <td><a href="update-single.php?id=<?php echo escape($row["id"]); ?>">Edit</a></td>
         </tr>
         <?php endforeach; ?>
     </tbody>

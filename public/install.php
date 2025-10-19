fd6b64c70bf8c445ad2af4805f61cf133b8ff971
sejungkwak <kwak.sejung@gmail.com>
2025-10-19 12:38:01 +0100

    Add a try-catch block

--- public/install.php
+++ public/install.php
@@ -1,7 +1,17 @@
+<?php
+
+/**
+  * Open a connection via PDO to create a new database and table with structure. 
+  */
+
 require "config.php";
 
-$connection = new PDO("mysql:host=$host", $username, $password, $options);
-
-$sql = file_get_contents("data/init.sql");
-$connection->exec($sql);
-Echo “DB setup”;
+try {    
+    $connection = new PDO("mysql:host=$host", $username, $password, $options);
+    
+    $sql = file_get_contents("data/init.sql");
+    $connection->exec($sql);
+    echo "Database and table users created successfully.";
+} catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+}

401425d761fd9b02fac29a7f11d3720ab511eef7
sejungkwak <kwak.sejung@gmail.com>
2025-10-19 15:59:39 +0100

    Add code to add a new user to the db

--- public/create.php
+++ public/create.php
@@ -1,6 +1,33 @@
 <?php include "templates/header.php"; ?>
 
 <h2>Add a user</h2>
+
+<?php 
+
+if (isset($_POST['submit'])) {
+
+    require "../config.php";
+
+    try {
+        $connection = new PDO($dsn, $username, $password, $options);
+        $new_user = array(
+            "firstname" => $_POST['firstname'],
+            "lastname" => $_POST['lastname'],
+            "email" => $_POST['email'],
+            "age" => $_POST['age'],
+            "location" => $_POST['location']
+        );
+        $sql = sprintf( "INSERT INTO %s (%s) values (%s)", "users", implode(", ",
+                array_keys($new_user)), ":" . implode(", :", array_keys($new_user)) );
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+
+?>
+
 <form method="post">
     <label for="firstname">First Name</label>
     <input type="text" name="firstname" id="firstname" required>

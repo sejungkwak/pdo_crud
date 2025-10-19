0e937569ecea3c1739a0eacafdbca028046851e3
sejungkwak <kwak.sejung@gmail.com>
2025-10-19 12:32:09 +0100

    Add code to execute init.sql

--- public/install.php
+++ public/install.php
@@ -1,3 +1,7 @@
 require "config.php";
 
 $connection = new PDO("mysql:host=$host", $username, $password, $options);
+
+$sql = file_get_contents("data/init.sql");
+$connection->exec($sql);
+Echo “DB setup”;
